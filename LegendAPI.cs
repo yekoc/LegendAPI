fa3bbd590f9e4d835703e3e1050d45e5edb994a3
yekoc <>
2022-05-03 01:15:01 +0300

    LegendAPI: Added logic to disable unused modules

--- LegendAPI.cs
+++ LegendAPI.cs
@@ -4,13 +4,26 @@ using BepInEx.Logging;
 using On;
 
 namespace LegendAPI {
-    [BepInPlugin("xyz.yekoc.wizardoflegend.LegendAPI", "Wizard of Legend API", "1.0.1")]
+    [BepInPlugin("xyz.yekoc.wizardoflegend.LegendAPI", "Wizard of Legend API", "1.1.0")]
     public class LegendAPI : BaseUnityPlugin {
         internal new static ManualLogSource Logger { get; set; }
-        public void Awake() {
+	public void Awake() {
             Logger = base.Logger;
             Items.Awake();
             Outfits.Awake();
+	    Elements.Awake();
+	    Utility.Hook();
         }
+	public void Start(){
+	  if(Outfits.OutfitCatalog.Count == 0){
+		Outfits.enabled = false;	
+	  }
+	  if(Items.ItemCatalog.Count == 0 && Items.RecipeCatalog.Count == 0 && Items.GroupCatalog.Count == 0){
+		Items.enabled = false;
+	  }
+	}
+	public void FixedUpdate(){
+	  Utility.CleanExtraAttackInfo();
+	}
     }
 }
